9a0b09f5e87dbc0488fc61ed3d62dc51573c5224
Todd H <github@simplybest.com>
2023-04-14 19:03:42 -0600

    add dark mode selector with system fallback

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, Platform } from '@ionic/angular';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonicModule, ExploreContainerComponent],
+  imports: [IonicModule, ExploreContainerComponent, RouterModule, FormsModule],
 })
 export class Tab1Page {
-  constructor() {}
+  
+  constructor(public platform: Platform) {}
+
+  mode = 'auto';
+
+  updateMode(event: Event) {
+    console.log(this.mode);
+    // document.body.classList.add(this.platform.is('ios') ? 'ios' : 'md');
+    document.body.classList.remove('dark');
+    document.body.classList.remove('light');
+    if (this.mode !== 'auto') {
+      document.body.classList.add(this.mode);
+    }
+  }
 }
